221f5d992e59b3f31cfa33ff35ea6126a1d96f37
Kiet <92447748+Sawashi@users.noreply.github.com>
2023-11-15 13:43:03 +0700

    AW01 | Update gaps

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,20 +1,20 @@
 import Head from "next/head";
 import Header from "components/Header";
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, Center, Image, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 const Home = () => {
   const [dataToShow, setDataToShow] = useState<any[]>([]);
 
   const generateRandomNumber = () => {
-    const randomNumber = Math.floor(Math.random() * 50) + 1;
+    const randomNumber = Math.floor(Math.random() * 20) + 1;
     return randomNumber;
   };
 
   const handleFetchDataRandom = async () => {
     const randomPage = generateRandomNumber();
     const response = await fetch(
-      `https://api.unsplash.com/photos?page=${randomPage}&per_page=30&client_id=pzObb_eHBRNoysOGQ9e1bmJ_z3w_W-p2Q7zEOGGx7m8`
+      `https://api.unsplash.com/photos?page=${randomPage}&per_page=20&client_id=pzObb_eHBRNoysOGQ9e1bmJ_z3w_W-p2Q7zEOGGx7m8`
     );
     const data = await response.json();
     return data;
@@ -34,9 +34,6 @@ const Home = () => {
   return (
     <div>
       <Header />
-      <Text fontSize="4xl" sx={{ margin: "2%" }}>
-        Here are some wonderful images for today!
-      </Text>
       <InfiniteScroll
         dataLength={dataToShow.length}
         next={handleLoadMore}
@@ -49,11 +46,11 @@ const Home = () => {
         }
       >
         <Box
-          padding={4}
+          padding={2}
           w="100%"
           maxW="80%"
           mx="auto"
-          sx={{ columnCount: [1, 2, 3, 4, 5], columnGap: "8px" }}
+          sx={{ columnCount: [1, 2, 3, 4, 5], columnGap: "2%" }}
         >
           {dataToShow.map((image) => (
             <Image
@@ -61,7 +58,7 @@ const Home = () => {
               w="100%"
               borderRadius="xl"
               mb={2}
-              sx={{ display: "inline-block" }}
+              sx={{ display: "inline-block", margin: "4% 0%" }}
               src={image.urls.raw}
               alt="Alt"
             />
