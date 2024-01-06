94c4f245363a68d3265c74ca1aa00085277a2a91
Kiet <92447748+Sawashi@users.noreply.github.com>
2024-01-06 14:23:29 +0700

    AW-75 | Fixed active+deactive class and show list of student in class

--- API/delete/delete.class.ts
+++ API/delete/delete.class.ts
@@ -1,10 +1,2 @@
 import { api } from "API";
 import { ClassesApiRouters } from "API/router.api";
-
-export const softDeleteClass = async (classId: string) => {
-  const response = await api.delete(
-    ClassesApiRouters.delete.soft_delete_class.value(classId)
-  );
-  if (response.status !== 200) throw new Error(response.statusText);
-  return response.data;
-};
